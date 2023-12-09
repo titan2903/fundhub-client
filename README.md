c0f6cd79443812c4ee7da25b192dbb8fd8990468
titan <titanioy98@gmail.com>
2023-12-09 21:52:07 +0700

    fundhub client

---
+++ README.md
@@ -0,0 +1,37 @@
+# crowdfunding-client
+
+## Build Setup
+
+* Install dependencies
+
+```sh
+    npm install
+```
+
+* Serve with hot reload at localhost:3010
+
+```sh
+    npm run dev
+```
+
+* Build for production and launch server
+
+```sh
+    npm run build
+```
+
+```sh
+    npm run start
+```
+
+* Generate static project
+
+```sh
+    npm run generate
+```
+
+## Web Address
+
+* [Click this link to view the web application](https://632d9087bb117102bdbf28b8--titan-crowdfunding-app.netlify.app/)
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ nuxt.config.js
@@ -0,0 +1,92 @@
+export default {
+  // Setup port server
+  server: {
+    port: 3010 // default: 3000
+  },
+
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'titan-crowdfunding',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' },
+
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://auth.nuxtjs.org/
+    '@nuxtjs/auth',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    baseURL: "https://mighty-taiga-99362.herokuapp.com",
+    proxyHeaders: false,
+    credentials: false
+  },
+
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: { url: '/api/v1/login', method: 'post', propertyName: 'data.token' },
+          logout: false,
+          user: { url: '/api/v1/users/fetch', method: 'get', propertyName: 'data'}
+        }
+      }
+    }
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  future: {
+    webpack5: true,
+  },
+  generate: {
+    fallback: true
+  },
+  buildDir: 'dist',
+}
