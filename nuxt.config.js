012423ff078b1d78ea717a0632471b1cc6d1e2ea
titan <titanioy98@gmail.com>
2023-12-21 13:34:52 +0700

    update axios config

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: process.env.BASE_URL || 'http://localhost:8000',
+    baseURL: process.env.BASE_URL || 'http://fundhub-api-dev-svc.backend.svc.cluster.local',
     // proxyHeaders: false,
     credentials: false
   },
