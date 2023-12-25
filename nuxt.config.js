7397a0591ce7eeb568f7ad3d2b748cc6d7901203
titan <titanioy98@gmail.com>
2023-12-25 11:15:54 +0700

    add base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.BASE_URL || 'http://fundhub.api.local',
   },
 
   auth: {
