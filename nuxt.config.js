cf4d7b2c29a9d71de85eafd2478d84b430d893de
titan <titanioy98@gmail.com>
2024-01-03 16:23:29 +0700

    change base url axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: process.env.ENV === "staging" ? 'https://fundhubdevapi.titanio.xyz/' : 'https://fundhubapi.titanio.xyz',
+    baseURL: process.env.ENV === "staging" ? 'https://fundhubdevapi.titanio.xyz' : 'https://fundhubapi.titanio.xyz',
   },
 
   auth: {
