d072f019c830075520e9d0ba2fafe729363d989f
titan <titanioy98@gmail.com>
2024-01-07 09:03:50 +0700

    fix indentation

--- components/CallToAction.vue
+++ components/CallToAction.vue
@@ -1,5 +1,5 @@
 <template>
-<section class="call-to-action bg-purple-progress pt-64 pb-10">
+  <section class="call-to-action bg-purple-progress pt-64 pb-10">
     <div class="container mx-auto">
       <div class="w-full text-center">
         <h1 class="text-5xl text-white font-semibold">
@@ -7,10 +7,8 @@
           <br />
           best idea and innovation
         </h1>
-        <button
-          @click="$router.push({ path: '/register' })"
-          class="inline-block bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-4 mt-8 text-lg rounded-full"
-        >
+        <button @click="$router.push({ path: '/register' })"
+          class="inline-block bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-4 mt-8 text-lg rounded-full">
           Getting Started
         </button>
       </div>
